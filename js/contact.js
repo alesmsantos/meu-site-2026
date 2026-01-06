a40824cb49064ff4773c3a830abcd104b0df4fa7
Alessandra Santos <alessandrasantosti@gmail.com>
2026-01-06 10:10:16 -0300

    Adicionado os arquivos JS e SCSS

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Vamos lá, você tem um nome, não tem?",
+                    minlength: "Seu nome deve conter pelo menos 2 caracteres."
+                },
+                subject: {
+                    required: "Vamos lá, você tem um assunto, não tem?",
+                    minlength: "O assunto deve conter pelo menos 4 caracteres."
+                },
+                number: {
+                    required: "Vamos lá, você tem um número, não tem?",
+                    minlength: "Seu número deve conter pelo menos 5 caracteres."
+                },
+                email: {
+                    required: "Sem e-mail, sem mensagem."
+                },
+                message: {
+                    required: "Hum... sim, você precisa escrever algo para enviar este formulário.",
+                    minlength: "Só isso? Sério?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
